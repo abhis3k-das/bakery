cc9bfe79f46f97222e9cd7df4f552c92d2fc172e
abhis3k <abhis3k.das@gmail.com>
2023-06-09 12:04:43 +0530

    created 2nd navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Items from './pages/Items/Items';
 import MainOutlet from './utils/MainOutlet';
+import HeaderOutlet from './utils/HeaderOutlet';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainOutlet />}>
         <Route path='home' element={<Home />}></Route>
-        <Route path='items' element={<Items/>}></Route>
+        <Route path='bakery' element={<HeaderOutlet/>}>
+          <Route path='items' element={<Items/>}></Route>
+          <Route path='about' element={<h1>Hello</h1>}/>
+          <Route path='contactUs' element={<h1>Hello</h1>}/>
+          <Route path='blog' element={<h1>Hello</h1>}/>
+          <Route path='login' element={<h1>Login/signup</h1>}></Route>
+          <Route path='signup' element={<h1>Login/signup</h1>}></Route>
+        </Route>
       </Route>
     </Routes>
   )
--- frontend/src/layouts/Header.js
+++ frontend/src/layouts/Header.js
@@ -1,22 +1,136 @@
-import { NavLink } from "react-router-dom";
-import styles from "./Header.module.css";
+import {NavLink, useLocation, useMatch} from "react-router-dom"
+import styles from "./Header.module.css"
+import logo from "../images/bakerylogo2.svg"
+import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai"
+import {useEffect, useState} from "react"
 function Header() {
-    return (
-        <div className={["container", styles.headers].join(" ")}>
-            <div className={styles.left}>
-                <NavLink to="/home" className={styles.navLink}>Bakery</NavLink>
-            </div>
-            <div className={styles.center}>
-                <NavLink to="/home" className={({ isActive }) => [styles.navLink, isActive ? styles.isActive : ""].join(" ")}>Home</NavLink>
-                <NavLink to="/about" className={({ isActive }) => [styles.navLink, isActive ? styles.isActive : ""].join(" ")}>About</NavLink>
-                <NavLink to="/produts" className={({ isActive }) => [styles.navLink, isActive ? styles.isActive : ""].join(" ")}>Product</NavLink>
-                <NavLink to="/blog" className={({ isActive }) => [styles.navLink, isActive ? styles.isActive : ""].join(" ")}>Blog</NavLink>
-                <NavLink to="/contact" className={({ isActive }) => [styles.navLink, isActive ? styles.isActive : ""].join(" ")}>Contact</NavLink>
-            </div>
-            <div className={styles.right}>
-                <NavLink to="/home" className={styles.navLink}>Cart</NavLink>
-            </div>
-        </div>
-    )
+	const [menu, setMenu] = useState(true)
+	const [slider, setSlider] = useState()
+	const location = useLocation()
+	useEffect(() => {
+		const handleSlidingMenuReset = () => {
+			setMenu(true)
+		}
+		window.addEventListener("resize", handleSlidingMenuReset)
+		return () => {
+			window.removeEventListener("resize", handleSlidingMenuReset)
+		}
+	}, [])
+	useEffect(() => {
+		console.log(location)
+		const routeSperator = location.pathname.split("/")
+		const route = routeSperator[routeSperator.length - 1]
+		if (route === "items") {
+			setSlider(140)
+		} else if (route === "blog") {
+			setSlider(280)
+		} else if (route === "about") {
+			setSlider(420)
+		} else if (route === "contactUs") {
+			setSlider(560)
+		}else{
+            setSlider(undefined)
+        }
+	}, [])
+	return (
+		<nav className={styles["navbar-container"]}>
+			<div className={styles["navbar-logo-container"]}>
+				<img
+					src={logo}
+					alt="..."
+					className={styles["navbar-logo"]}
+				></img>
+			</div>
+			<div className={styles["navbar-links"]}>
+				<div className={styles["navbar-left-links"]}>
+					<AiOutlineMenu
+						className={[styles["menu-icon"], menu === true ? styles["show"] : styles["close"]].join(" ")}
+						onClick={() => setMenu(false)}
+					/>
+					<AiOutlineClose
+						className={[styles["menu-close"], menu === false ? styles["show"] : styles["close"]].join(" ")}
+						onClick={() => setMenu(true)}
+					/>
+					<div className={[styles["navbar-menu-links"], menu === false ? styles["show"] : styles["close"]].join(" ")}>
+						<div className={styles["links"]}>
+							<NavLink
+								onClick={() => setSlider(0)}
+								to="/home"
+								className={({isActive}) => {
+									return isActive ? styles["activeLink"] : ""
+								}}
+							>
+								Home
+							</NavLink>
+						</div>
+						<div className={styles["links"]}>
+							<NavLink
+								onClick={() => setSlider(140 * 1)}
+								to="items"
+								className={({isActive}) => {
+									return isActive ? styles["activeLink"] : ""
+								}}
+							>
+								Products
+							</NavLink>
+						</div>
+						<div className={styles["links"]}>
+							<NavLink
+								onClick={() => setSlider(140 * 2)}
+								to="blog"
+								className={({isActive}) => {
+									return isActive ? styles["activeLink"] : ""
+								}}
+							>
+								Blog
+							</NavLink>
+						</div>
+						<div className={styles["links"]}>
+							<NavLink
+								onClick={() => setSlider(140 * 3)}
+								to="about"
+								className={({isActive}) => {
+									return isActive ? styles["activeLink"] : ""
+								}}
+							>
+								About Us
+							</NavLink>
+						</div>
+						<div className={styles["links"]}>
+							<NavLink
+								onClick={() => setSlider(140 * 4)}
+								to="contactUs"
+								className={({isActive}) => {
+									return isActive ? styles["activeLink"] : ""
+								}}
+							>
+								Contact us
+							</NavLink>
+						</div>
+						<div className={[styles["slider"], styles["start-home"]].join(" ")}></div>
+						{slider && (
+							<div
+								className={[styles["slider"]].join(" ")}
+								style={{left: `${slider}px`, width: "140px", backgroundColor: "#DCDCDC"}}
+							></div>
+						)}
+					</div>
+				</div>
+				<div className={styles["navbar-right-links"]}>
+					<div className={styles["links"]}>
+						<NavLink
+							to="signUp"
+							onClick={() => setSlider(undefined)}
+						>
+							SignUp
+						</NavLink>
+					</div>
+					<div className={styles["links"]}>
+						<NavLink to="login">Login</NavLink>
+					</div>
+				</div>
+			</div>
+		</nav>
+	)
 }
-export default Header;
+export default Header
