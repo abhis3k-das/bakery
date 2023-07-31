52019e7804f417f2e8a093b6336576bb7c513f4a
abhis3k <abhis3k.das@gmail.com>
2023-07-31 15:20:13 +0530

    --ammend

--- backend/cors/index.js
+++ backend/cors/index.js
@@ -1,7 +1,7 @@
 const allowedLinks = ["http://localhost:3000","https://glistening-klepon-5eae84.netlify.app","https://bakery-315a.onrender.com"]
 const cors = require('cors')({
     optionSuccessStatus:true,
-    credentials:false,
+    credentials:true,
     origin:(origin,callback)=>{
         if(allowedLinks.indexOf(origin) !== -1 || !origin){
             callback(null,true);
